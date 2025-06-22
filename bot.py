55a0029902bef95454418032aa0ea89e0c1e35a0
dydx06 <dhyan.laad@gmail.com>
2025-06-22 20:17:53 +0530

    Add cogs structure

--- bot.py
+++ bot.py
@@ -4,12 +4,13 @@ from dotenv import load_dotenv
 import os
 
 intents = discord.Intents.default()
-intents.message_content = True  # Needed to receive messages
 bot = commands.Bot(command_prefix='#', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} online')
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"): await bot.load_extension(f"cogs.{filename[:-3]}")
 
 @bot.command()
 async def ping(ctx):
